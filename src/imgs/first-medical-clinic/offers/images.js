18a7fd68ced9044cf6548eea5e8ae924ccd5ab57
Mamdouh <mamdouh.mohammed919@gmail.com>
2024-12-08 09:43:20 +0200

    add offers to first-medical-clinic

---
+++ src/imgs/first-medical-clinic/offers/images.js
@@ -0,0 +1,12 @@
+import offer1 from "./offer-1.jpg";
+import offer2 from "./offer-2.jpg";
+import offer3 from "./offer-3.jpg";
+
+// Returning them in an array
+const menuImages = [
+  offer1,
+  offer2,
+  offer3,
+];
+
+export default menuImages;
