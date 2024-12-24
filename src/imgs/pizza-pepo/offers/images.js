8de697aa16969e6fcc2ec24cade65c0cd9c62317
Mamdouh <mamdouh.mohammed919@gmail.com>
2024-12-24 09:09:49 +0200

    updates

--- src/imgs/pizza-pepo/offers/images.js
+++ src/imgs/pizza-pepo/offers/images.js
@@ -2,13 +2,17 @@ import offer1 from "./offer-1.jpg";
 import offer2 from "./offer-2.jpg";
 import offer3 from "./offer-3.jpg";
 import offer4 from "./offer-4.jpg";
+import offer5 from "./offer-5.jpg";
+import offer6 from "./offer-6.jpeg";
 
 // Returning them in an array
 const menuImages = [
   offer1,
   offer2,
   offer3,
-  offer4
+  offer4,
+  offer5,
+  offer6
 ];
 
 export default menuImages;
