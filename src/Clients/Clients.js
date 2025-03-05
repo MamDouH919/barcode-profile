f1c1e8eac6b43d5c25134a23c4a71ea93839d23f
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-03-05 10:30:45 +0200

    update baitward

--- src/Clients/Clients.js
+++ src/Clients/Clients.js
@@ -4,17 +4,29 @@ import NotFound from '../NotFound';
 // import InfoQr from './typesOfQr/InfoQr';
 import MenuQr from './typesOfQr/MenuQr';
 import QrCodeProfile from './typesOfQr/QrCodeProfile';
-import { Suspense, useState, useEffect } from "react";
+import { Suspense, useState, useEffect, useLayoutEffect } from "react";
 
 function Clients({ idDomain }) {
     const { id } = useParams()
     const client = clients[id ?? idDomain]
+    useLayoutEffect(() => {
+        if (id === "bait-ward") {
+            window.location.href = "https://betward.city-app.org/loading";
+        }
+
+        return () => { };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     if (!client || client.disable) return <NotFound />
+
+
 
     if (client.customComp) {
         return <DynamicComponent compName={client.customComp} client={client} id={id} haveDomain={!!idDomain} />;
     }
+
+
 
     return client.menuQr ? (<MenuQr client={client} id={id} />) : (
         <QrCodeProfile client={client} id={id} haveDomain={!!idDomain} />
