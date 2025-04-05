b15de61c3398df4208a7158b6bc509dddd2723f2
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-04-05 23:23:26 +0200

    update pizza pepo and bar mousa

--- src/imgs/pizza-pepo/offers/images.js
+++ src/imgs/pizza-pepo/offers/images.js
@@ -1,18 +1,8 @@
-import offer1 from "./offer-1.jpg";
-import offer2 from "./offer-2.jpg";
-import offer3 from "./offer-3.jpg";
-import offer4 from "./offer-4.jpg";
-import offer5 from "./offer-5.jpg";
-import offer6 from "./offer-6.jpeg";
+import offer1 from "./offer1.jpeg";
 
 // Returning them in an array
 const menuImages = [
   offer1,
-  offer2,
-  offer3,
-  offer4,
-  offer5,
-  offer6
 ];
 
 export default menuImages;
