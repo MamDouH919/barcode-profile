9cfb52a00631f8fe089664a8623479e7ef6bc778
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-07-27 18:38:45 +0300

    updates

--- src/imgs/bar-mousa/barMousa/images.js
+++ src/imgs/bar-mousa/barMousa/images.js
@@ -1,8 +1,10 @@
 import menu1 from "./menu1.jpeg";
+import menu2 from "./menu2.jpeg";
 
 // Returning them in an array
 const menuImages = [
-  menu1
+  menu1,
+  menu2
 ];
 
 export default menuImages;
