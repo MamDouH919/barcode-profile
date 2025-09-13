f7cabd549a9256a4176c05ed94b9eefcb6a97fab
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-09-13 17:30:41 +0300

    updates

---
+++ src/imgs/pizza-pepo/barMousa/images.js
@@ -0,0 +1,26 @@
+import menu1 from "./menu1.jpeg";
+import menu2 from "./menu2.jpeg";
+import menu3 from "./menu3.jpeg";
+import menu4 from "./menu4.jpeg";
+import menu5 from "./menu5.jpeg";
+import menu6 from "./menu6.jpeg";
+import menu7 from "./menu7.jpeg";
+import menu8 from "./menu8.jpeg";
+import menu9 from "./menu9.jpeg";
+import menu10 from "./menu10.jpeg";
+
+// Returning them in an array
+const menuImages = [
+  menu1,
+  menu2,
+  menu3,
+  menu4,
+  menu5,
+  menu6,
+  menu7,
+  menu8,
+  menu9,
+  menu10,
+];
+
+export default menuImages;
